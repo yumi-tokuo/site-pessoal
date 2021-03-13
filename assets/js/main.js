3b606956c7491e48a3ba99347a6a79077eec1280
Yumi <yumitokuo@gmail.com>
2021-03-13 16:00:05 -0300

    teste js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,14 @@
 		xsmall:	'(max-width: 480px)'
 	});
 
+	<input id="night-mode" class="lamp" type="checkbox" aria-label="night-mode" />
+	const nightMode = document.querySelector('#night-mode')
+	// ao clicar mudaremos as cores
+	nightMode.addEventListener('click', () => {
+	  // adiciona a classe `night-mode` ao html
+	  document.documentElement.classList.toggle('night-mode')
+	})
+
 	$(function() {
 
 		var	$window = $(window),
