ac77d48f8c6ff4263382b661eb958c0224551669
Yumi <yumitokuo@gmail.com>
2021-03-14 21:50:22 -0300

    js teste

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-const $html = document.querySelector('html')
-const $checkbox = document.querySelector('#switch')
-
-$checkbox.addEventListener('change', function(){
-	$html.classList.troggle('dark-mode')
-})
-
 (function($) {
 
 	skel.breakpoints({
